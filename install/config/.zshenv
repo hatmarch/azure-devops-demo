33c7a1b6fe9a9a467d31f982e6d83b388fba3605
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-12-12 13:45:19 +0000

    Add environment file for eShopWeb
    Connection string secret is substituted in from create-demo -q option
    
    Environment files are the basis for secrets for use in the demo and CRW
    
    Update readme instructions for demo creation
    
    Ignore the $DEMO_HOME/secrets directory
    
    Update launch.json to use the environment files

---
+++ install/config/.zshenv
@@ -0,0 +1,4 @@
+ConnectionStrings__CatalogConnection=Server=localhost; UID=sa; Password=@SA_PASSWORD@; Integrated Security=False; Initial Catalog=Microsoft.eShopOnWeb.CatalogDb;
+ConnectionStrings__IdentityConnection=Server=localhost; UID=sa; Password=@SA_PASSWORD@; Integrated Security=False; Initial Catalog=Microsoft.eShopOnWeb.Identity;
+ASPNETCORE_ENVIRONMENT=Development
+ASPNETCORE_URLS=http://*:8080
--- scripts/create-demo.sh
+++ scripts/create-demo.sh
@@ -4,6 +4,7 @@ set -Eeuo pipefail
 
 declare -r SCRIPT_DIR=$(cd -P $(dirname $0) && pwd)
 declare PROJECT_PREFIX="az-demo"
+declare SA_PASSWORD=""
 
 display_usage() {
 cat << EOF
@@ -13,6 +14,7 @@ $0: Azure DevOps Demo --
   
     -i         [optional] Install prerequisites
     -p <TEXT>  [optional] Project prefix to use.  Defaults to "az-demo"
+    -q <TEXT>  The password to use for the sa user of the database
 
 EOF
 }
@@ -30,10 +32,11 @@ get_and_validate_options() {
 
   
   # parse options
-  while getopts ':ip:h' option; do
+  while getopts ':ip:q:h' option; do
       case "${option}" in
           i  ) prereq_flag=true;;
           p  ) p_flag=true; PROJECT_PREFIX="${OPTARG}";;
+          q  ) q_flag=true; SA_PASSWORD="${OPTARG}";;
           h  ) display_usage; exit;;
           \? ) printf "%s\n\n" "  Invalid option: -${OPTARG}" >&2; display_usage >&2; exit 1;;
           :  ) printf "%s\n\n%s\n\n\n" "  Option -${OPTARG} requires an argument." >&2; display_usage >&2; exit 1;;
@@ -43,6 +46,12 @@ get_and_validate_options() {
 
   if [[ -z "${PROJECT_PREFIX}" ]]; then
       printf '%s\n\n' 'ERROR - PROJECT_PREFIX must not be null' >&2
+      display_usage >&2
+      exit 1
+  fi
+
+  if [[ -z "${SA_PASSWORD}" ]]; then
+      printf '%s\n\n' 'ERROR - Database sa password must not be null' >&2
       display_usage >&2
       exit 1
   fi
@@ -86,16 +95,23 @@ main() {
 
     echo "Deploying Database"
     oc get secret sql-secret -n $dev_prj 2>/dev/null || {
-        oc create secret generic sql-secret --from-literal SA_PASSWORD='yourStrong(!)Password' -n $dev_prj
+        oc create secret generic sql-secret --from-literal SA_PASSWORD="${SA_PASSWORD}" -n $dev_prj
     }
     # install this template to the openshift project so that it's available everywhere
     oc apply -f $DEMO_HOME/install/kube/database/database-template.yaml -n openshift
 
     # actually create the database (FIXME: Do this via the template)
     oc apply -f $DEMO_HOME/install/kube/database/database-deploy.yaml -n $dev_prj
+    echo -n "Waiting for database deployment to appear..."
+    while [[ -z "$(oc get deploy hplus-db -n $dev_prj 2>/dev/null)" ]]; do
+        echo -n "."
+        sleep 1
+    done
+    echo "done!"
+    oc rollout status deploy/hplus-db -n $dev_prj
 
     echo "Initializing database"
-#    ${SCRIPT_DIR}/initialize-database.sh $dev_prj
+    ${SCRIPT_DIR}/initialize-database.sh $dev_prj
 
     echo "Installing CodeReady Workspaces"
     ${SCRIPT_DIR}/install-crw.sh codeready
@@ -128,6 +144,32 @@ main() {
     oc create -f $DEMO_HOME/install/kube/gitea/gitea-init-taskrun.yaml -n $sup_prj
     # output the logs of the latest task
     tkn tr logs -L -f -n $sup_prj
+
+    crw_user_ns="$(oc whoami)-codeready"
+    oc get ns $crw_user_ns 2>/dev/null  || { 
+        oc new-project $crw_user_ns
+    }
+
+    echo "Writing secrets into environment templates..."
+    if [[ ! -d $DEMO_HOME/secrets ]]; then
+        mkdir $DEMO_HOME/secrets
+    fi
+    ENV_TEMPLATES=( .zshenv .zshenv-k8 )
+    for TEMPLATE in ${ENV_TEMPLATES[@]}; do
+        echo "Writing $DEMO_HOME/secrets/$TEMPLATE"
+        sed "s/@SA_PASSWORD@/${SA_PASSWORD}/" $DEMO_HOME/install/config/$TEMPLATE > $DEMO_HOME/secrets/$TEMPLATE
+    done
+    echo "done."
+
+    echo "Pre-seed the user codeready namespace (${crw_user_ns}) with environment secret"
+    oc create secret generic codeready-env --from-file=.zshenv=$DEMO_HOME/secrets/.zshenv-k8 -n $crw_user_ns
+
+    # annotate the secret so that it is loaded by default in all codeready workspaces
+    oc annotate secret --overwrite codeready-env "che.eclipse.org/automount-workspace-secret"="true" "che.eclipse.org/mount-path"="{prod-home}" \
+        "che.eclipse.org/mount-as"="file" -n $crw_user_ns
+
+    echo "Create secret for deployment config in dev environment"
+    oc create secret generic eshop-dev --from-env-file=$DEMO_HOME/secrets/.zshenv-k8 -n $dev_prj
 
     echo "Demo installation completed successfully!"
 }
